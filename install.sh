a49d19ec66c213f9625092ab8565cb3d5da3ddbf
gmmirror <blackstar76776@gmail.com>
2026-04-02 05:40:09 +0700

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-[[ -d "${PREFIX}/tmp/muxly" ]] && {
+if [[ ! -d "${PREFIX}/tmp" ]]; then
+    command mkdir -p "${PREFIX}/tmp"
+fi
+
+if [[ -d "${PREFIX}/tmp/muxly" ]]; then
     command rm -rf "${PREFIX}/tmp/muxly"
-}
+fi
 
 command git clone --depth 1 \
     'https://github.com/Zeronetsec/Muxly' \
@@ -12,5 +16,3 @@ cd "${PREFIX}/tmp/muxly" || exit 1
 command chmod +x -R "install.sh"
 command bash "install.sh"
 cd
-
-exit 0
